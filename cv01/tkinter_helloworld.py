0706d10ed6b38226e814a2e92834c526f2b5423b
Daniel Madera <madera.dan@gmail.com>
2016-03-01 07:32:11 +0000

    cv01 - added button

--- cv01/tkinter_helloworld.py
+++ cv01/tkinter_helloworld.py
@@ -3,11 +3,18 @@
 import Tkinter
 
 
+def button_clicked():
+    print "Button clicked!"
+
+
 def main():
     app = Tkinter.Tk()
 
     label = Tkinter.Label(app, text="Hello, world")
     label.pack()
+
+    button = Tkinter.Button(app, text="Ok!", command=button_clicked)
+    button.pack()
 
     app.mailoop()
 
